e5b41ee083b6bbc9b74381c2e3436535fc6eb51e
gulshoda10 <gulichka_rd@yahoo.com>
2021-06-12 10:12:51 -0700

    the final red audi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         car audi=new car();
+        audi.color="red";
+        audi.number="";
+        audi.door="3";
+        audi.model="A3";
 
     }
 }
